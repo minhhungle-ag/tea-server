074426ffcc369bdaf3963f64ed540c1123ea48e3
minhhungle <haless132@gmail.com>
2023-06-25 13:58:13 +0700

    type

--- api/controllers/productController.js
+++ api/controllers/productController.js
@@ -5,10 +5,13 @@ const productController = {
   async getAll(req, res) {
     const page = parseInt(req.query.page) || 1
     const limit = parseInt(req.query.limit) || 10
+    const type = req.query.type
+    const title = req.query.title
+
     const skip = (page - 1) * limit
 
     try {
-      const productList = await Product.find()
+      const productList = await Product.find({ type, title })
         .sort({ createdAt: -1 })
         .skip(skip)
         .limit(limit)
